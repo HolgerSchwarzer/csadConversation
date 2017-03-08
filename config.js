04ba44992ba4f9e3ce9fa034f588b202c3e7099b
Holger Schwarzer <holgerschwarzer@Holgers-Mac-Book-Pro.local>
2017-03-08 16:41:22 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+	 watson: {
+	   conversation: {
+	      username: "e5ff6e30-3ef0-409a-b249-bf09ad934fb9",   
+	      password: "iVmJeIQ7732a",   
+	      version: "v1",   
+	      version_date: "2016-09-20",   
+	      workspace_id: "89f8e257-8bfa-4e5c-b21a-fb328cc3c278"  
+	  } 
+	}
+};
+module.exports = config;
